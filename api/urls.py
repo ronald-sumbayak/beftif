22d68533c67db74b77ef1462d16334c0dc023b30
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2017-10-03 19:16:13 +0700

    api test fixed final

--- api/urls.py
+++ api/urls.py
@@ -8,8 +8,8 @@ router = routers.SimpleRouter ()
 router.register (r'categories', CategoryViewSet)
 
 urlpatterns = [
-    url (r'^$', views.test),
-    url (r'^news/$', NewsList.as_view ()),
+    url (r'^test$', views.test),
+    url (r'^news$', NewsList.as_view ()),
     url (r'^news/(?P<pk>[\d]+)$', RetrieveNews.as_view ()),
     url (r'^media/', include ('api.media.urls'))
 ]
